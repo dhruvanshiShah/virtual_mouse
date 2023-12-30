33880247197c6fae1d510672222115b5df46680a
dhruvanshiShah <136938318+dhruvanshiShah@users.noreply.github.com>
2023-12-30 23:45:37 +0530

    Update hand_detection.py

--- hand_detection.py
+++ hand_detection.py
@@ -1,98 +1,95 @@
-"""
-Hand Tracking Module
-By: Murtaza Hassan
-Youtube: http://www.youtube.com/c/MurtazasWorkshopRoboticsandAI
-Website: https://www.computervision.zone
-"""
-
 import cv2
 import mediapipe as mp
 import time
 import math
 import numpy as np
 
-class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+class HandDetector:
+    def __init__(self, mode=False, max_hands=2, detection_con=0.5, track_con=0.5):
+        # Initialize HandDetector with configuration parameters
         self.mode = mode
-        self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(static_image_mode=self.mode,
-            max_num_hands=self.maxHands,
-            min_detection_confidence=self.detectionCon,
-            min_tracking_confidence=self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
-        self.tipIds = [4, 8, 12, 16, 20]
+        self.max_hands = max_hands
+        self.detection_con = detection_con
+        self.track_con = track_con
+        self.mp_hands = mp.solutions.hands
+        # Initialize MediaPipe Hands module
+        self.hands = self.mp_hands.Hands(static_image_mode=self.mode,
+                                         max_num_hands=self.max_hands,
+                                         min_detection_confidence=self.detection_con,
+                                         min_tracking_confidence=self.track_con)
+        self.mp_draw = mp.solutions.drawing_utils
+        self.tip_ids = [4, 8, 12, 16, 20]
 
-    def findHands(self, img, draw=True):
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
+    def find_hands(self, img, draw=True):
+        # Convert image to RGB
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # Process the image with the Hand module
+        results = self.hands.process(img_rgb)
 
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
+        if results.multi_hand_landmarks:
+            # Draw landmarks and connections if hands are detected
+            for hand_lms in results.multi_hand_landmarks:
                 if draw:
-                    DrawingSpec = self.mpDraw.DrawingSpec
-                    landmark_drawing_spec = DrawingSpec(color=(0, 128, 0))
-                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS,
-                                               landmark_drawing_spec=landmark_drawing_spec,
-                                               connection_drawing_spec=DrawingSpec(color=(0, 0, 180)))
+                    drawing_spec = self.mp_draw.DrawingSpec
+                    landmark_drawing_spec = drawing_spec(color=(0, 128, 0))
+                    self.mp_draw.draw_landmarks(img, hand_lms, self.mp_hands.HAND_CONNECTIONS,
+                                                landmark_drawing_spec=landmark_drawing_spec,
+                                                connection_drawing_spec=drawing_spec(color=(0, 0, 180)))
 
         return img
 
-    def findPosition(self, img, handNo=0, draw=True):
-        xList = []
-        yList = []
+    def find_position(self, img, hand_no=0, draw=True):
+        x_list = []
+        y_list = []
         bbox = []
-        self.lmList = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
+        lm_list = []
+
+        if self.hands.results.multi_hand_landmarks:
+            # Extract information about the detected hand
+            my_hand = self.hands.results.multi_hand_landmarks[hand_no]
+            for id, lm in enumerate(my_hand.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                xList.append(cx)
-                yList.append(cy)
-                # print(id, cx, cy)
-                self.lmList.append([id, cx, cy])
+                x_list.append(cx)
+                y_list.append(cy)
+                lm_list.append([id, cx, cy])
+
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (0, 128, 0), cv2.FILLED)
 
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
+            xmin, xmax = min(x_list), max(x_list)
+            ymin, ymax = min(y_list), max(y_list)
             bbox = xmin, ymin, xmax, ymax
 
             if draw:
+                # Draw bounding box around the hand
                 cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
-                (0, 255, 0), 2)
-        return self.lmList, bbox
+                              (0, 255, 0), 2)
+        return lm_list, bbox
 
-    def fingersUp(self):
+    def fingers_up(self):
         fingers = []
-        # Thumb
-        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+        # Check if each finger is up or down
+        if self.lm_list[self.tip_ids[0]][1] > self.lm_list[self.tip_ids[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
 
-        # Fingers
         for id in range(0, 4):
-            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+            if self.lm_list[self.tip_ids[id]][2] < self.lm_list[self.tip_ids[id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
 
-            # totalFingers = fingers.count(1)
-
         return fingers
 
-    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
+    def find_distance(self, p1, p2, img, draw=True, r=15, t=3):
+        x1, y1 = self.lm_list[p1][1:]
+        x2, y2 = self.lm_list[p2][1:]
         cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
         if draw:
+            # Draw line and circles to represent distance
             cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
             cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
             cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
@@ -102,26 +99,7 @@ class handDetector():
         return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
-    pTime = 0
-    cTime = 0
+    p_time = 0
+    c_time = 0
     cap = cv2.VideoCapture(0)
     print("WebCam turned on and recording")
-    detector = handDetector()
-    while True:
-        success, img = cap.read()
-        img = cv2.flip(img, 1)
-        img = detector.findHands(img)
-        lmList, bbox = detector.findPosition(img)
-        cTime = time.time()
-        fps = 1 / (cTime - pTime)
-        pTime = cTime
-
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-        (255, 0, 255), 3)
-
-        cv2.imshow("Image", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-if __name__ == "__main__":
-    main()
